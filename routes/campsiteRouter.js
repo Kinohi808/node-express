b4d05dcd629ad57f0ea6440fcdf4b6fc0d25d7de
Kekai Newman <kekainewman@gmail.com>
2023-11-25 17:07:03 -1000

    Workshop 1 Router

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,5 +21,25 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details for the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite with name: ${req.body.name} and description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 module.exports = campsiteRouter;
